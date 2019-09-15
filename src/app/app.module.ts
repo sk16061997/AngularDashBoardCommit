f43091c080437179a21dc0a207b045d7f8a2cd68
Sudesh Kamble <kamblest16061997@gmail.com>
2019-09-15 11:19:37 -0700

    Revert "Add Component"
    This reverts commit d9ba349604b03e0844eff7ed1a43587dab1e5b6f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatSelectModule } from '@angular/material/select';
-import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-
 
 import { AppComponent } from './app.component';
 
@@ -16,15 +8,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    BrowserAnimationsModule,
-    NoopAnimationsModule,
-    MatIconModule,
-    MatSelectModule,
-    MatButtonModule,
-    MatTableModule,
-    MatRadioModule,
-    MatCheckboxModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
